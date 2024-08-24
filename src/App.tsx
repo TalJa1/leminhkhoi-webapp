46fc7c11d6765ff7c2abb80bdcc233487fd9bdc4
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-08-24 16:56:38 +0700

    refactor: Update routing in SignIn component to use react-router-dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignIn from "./components/SignIn";
+import SignIn from "./views/SignIn";
 import Home from "./views/Home";
 
 function App() {
